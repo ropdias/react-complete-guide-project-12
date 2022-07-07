be8976c68b202415faf7e459857505237bfca2a2
Rodrigo P. Dias <rodrigopdias@hotmail.com>
2022-07-06 22:41:29 -0300

    Adding an email input with validation

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -4,14 +4,20 @@ const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState("");
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
 
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredEmailTouched, setEnteredEmailTouched] = useState(false);
+
   // We can change the enteredNameIsValid state for a variable that is checked everytime the component
   // is re-rendered
   const enteredNameIsValid = enteredName.trim() !== "";
   const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
 
+  const enteredEmailIsValid = enteredEmail.includes('@');
+  const emailInputIsInvalid = !enteredEmailIsValid && enteredEmailTouched;
+
   let formIsValid = false;
 
-  if (enteredNameIsValid) { // We should add here all the inputs in the form
+  if (enteredNameIsValid && enteredEmailIsValid) { // We should add here all the inputs in the form
     formIsValid = true;
   }
 
@@ -20,8 +26,16 @@ const SimpleInput = (props) => {
     setEnteredName(event.target.value);
   };
 
+  const emailInputChangeHandler = (event) => {
+    setEnteredEmail(event.target.value);
+  };
+
   const nameInputBlurHandler = (event) => {
     setEnteredNameTouched(true);
+  };
+
+  const emailInputBlurHandler = (event) => {
+    setEnteredEmailTouched(true);
   };
 
   // This function will be recreated everytime the component re-renders and enteredNameIsValid will be re-evaluated
@@ -30,21 +44,29 @@ const SimpleInput = (props) => {
 
     // If the form is submited all the inputs are treated as touched
     setEnteredNameTouched(true);
+    setEnteredEmailTouched(true);
 
-    if (!enteredNameIsValid) {
+    if (!enteredNameIsValid || !enteredEmailIsValid) {
       return;
     }
 
     console.log(enteredName);
+    console.log(enteredEmail);
 
     // nameInputRef.current.value = ""; // It's not good to manipulate the DOM directly with vanilla JS code
     setEnteredName(""); // this resets the input value
     setEnteredNameTouched(false); // this makes the input not show as invalid when the form is submited
+    setEnteredEmail("");
+    setEnteredEmailTouched(false);
   };
 
   const nameInputClasses = nameInputIsInvalid
     ? "form-control invalid"
     : "form-control";
+
+  const emailInputClasses = emailInputIsInvalid
+  ? "form-control invalid"
+  : "form-control";
 
   return (
     <form onSubmit={formSubmissionHandler}>
@@ -59,6 +81,19 @@ const SimpleInput = (props) => {
         />
         {nameInputIsInvalid && (
           <p className="error-text">Name must not be empty.</p>
+        )}
+      </div>
+      <div className={emailInputClasses}>
+        <label htmlFor="email">Your Email address</label>
+        <input
+          type="email"
+          id="email"
+          onChange={emailInputChangeHandler}
+          onBlur={emailInputBlurHandler}
+          value={enteredEmail}
+        />
+        {emailInputIsInvalid && (
+          <p className="error-text">Please enter a valid email</p>
         )}
       </div>
       <div className="form-actions">
